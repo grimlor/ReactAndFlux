e2313d4f34a8d9c861b567c5698422d95ca0b804
Jack Pines <jackpi@microsoft.com>
2015-12-08 13:41:49 -0800

    Completed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -27,3 +27,11 @@ gulp.task('open', ['connect'], function() {
 	gulp.src('dist/index.html')
 		.pipe(open({ uri: config.devBaseUrl + ':' + config.port + '/' }));
 });
+
+gulp.task('html', function() {
+	gulp.src(config.paths.html)
+		.pipe(gulp.dest(config.paths.dist))
+		.pipe(connect.reload());
+});
+
+gulp.task('default', ['html', 'open'])
