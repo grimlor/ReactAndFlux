8209fef229146d95511c9e1dd7fdf1838f76e5cb
Jack Pines <jackpi@microsoft.com>
2015-12-11 12:13:45 -0800

    Switched to JSX files for all with markup. Completed module 4. Starting module 5.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
 "use strict";
 
 var gulp = require('gulp');
-var connect = require('gulp-connect');	// Runs a local dev server
-var open = require('gulp-open');		// Open a URL in a web browser
+var connect = require('gulp-connect');			// Runs a local dev server
+var open = require('gulp-open');				// Open a URL in a web browser
+var browserify = require('browserify');			// Bundles JS
+var reactify = require('reactify');				// Transforms React JSX to JS
+var source = require('vinyl-source-stream');	// Use conventional text streams with Gulp
+var concat = require('gulp-concat');			// Concatenates files
+var lint = require('gulp-eslint');				// Lint JS files, including JSX
 
 var config = {
 	port: 9005,
 	devBaseUrl: 'http://localhost',
 	paths: {
 		html: './src/*.html',
-		dist: './dist'
+		js:   './src/**/*.js*',
+		images: './src/images/*',
+		css:  ['node_modules/bootstrap/dist/css/bootstrap.min.css',
+			   'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'],
+		dist: './dist',
+		mainJs: './src/main.jsx'
 	}
 };
 
@@ -34,4 +44,41 @@ gulp.task('html', function() {
 		.pipe(connect.reload());
 });
 
-gulp.task('default', ['html', 'open'])
+gulp.task('js', function() {
+	browserify(config.paths.mainJs)
+		.transform(reactify)
+		.bundle()
+		.on('error', console.error.bind(console))
+		.pipe(source('bundle.js'))
+		.pipe(gulp.dest(config.paths.dist + '/scripts'))
+		.pipe(connect.reload());
+});
+
+gulp.task('images', function() {
+	gulp.src(config.paths.images)
+		.pipe(gulp.dest(config.paths.dist + '/images'))
+		.pipe(connect.reload());
+		
+	gulp.src('./src/favicon.ico')
+		.pipe(gulp.dest(config.paths.dist));
+});
+
+gulp.task('css', function() {
+	gulp.src(config.paths.css)
+		.pipe(concat('bundle.css'))
+		.pipe(gulp.dest(config.paths.dist + '/css'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch(config.paths.html, ['html']);
+	gulp.watch(config.paths.js, ['js', 'lint']);
+	gulp.watch(config.paths.images, ['images']);
+});
+
+gulp.task('lint', function() {
+	return gulp.src(config.paths.js)
+		.pipe(lint({config: 'eslint.config.json'}))
+		.pipe(lint.format());
+});
+
+gulp.task('default', ['html', 'js', 'css', 'images', 'open', 'watch', 'lint'])
